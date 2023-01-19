a7d206e8227e2a16877301fed56464f06c6af7ed
accguy <accguy2190@naver.com>
2023-01-19 11:25:10 +0900

    v1.0.71

--- src/pages/Start.tsx
+++ src/pages/Start.tsx
@@ -1,6 +1,6 @@
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import React, {useCallback} from 'react';
-import {Pressable, Text, View, Button} from 'react-native';
+import { Text, View, StyleSheet, TouchableOpacity, Dimensions, } from 'react-native';
 import {RootStackParamList} from '../../App';
 
 type ScreenProps = NativeStackScreenProps<
@@ -15,13 +15,41 @@ function Start({navigation}: ScreenProps) {
   }, [navigation]);
   return (
     <>
-      <View>
-        <Button title="Login" onPress={() => navigation.navigate('Login')} />
-        <Text>or</Text>
-        <Button title="Create Account" onPress={toSignUp} />
+      <View style={{flex:1, backgroundColor: '#FFFFFF'}}>
+        <TouchableOpacity style={styles.btn1Align} activeOpacity={0.5} onPress={() => navigation.navigate('Login')}>
+            <Text style={styles.btnStyle}>Login</Text>
+          </TouchableOpacity>
+        <Text  style={{alignSelf: 'center'}}>or</Text>
+        <TouchableOpacity style={styles.btn2Align} activeOpacity={0.5} onPress={toSignUp}>
+            <Text style={styles.btnStyle}>Create Account</Text>
+          </TouchableOpacity>
       </View>
     </>
   );
 }
+
+const {width: WIDTH} = Dimensions.get('window');
+const styles = StyleSheet.create({
+  btn1Align: {
+    alignSelf: 'center',
+    marginTop: 450,
+    marginBottom: 5,
+  },btn2Align: {
+    alignSelf: 'center',
+    marginTop: 10,
+    marginBottom: 5,
+  },
+  btnStyle: {
+    backgroundColor: '#6A74CF',
+    width: WIDTH * 0.7,
+
+    height: 40,
+    color: 'white',
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    borderRadius: 77,
+  },
+
+});
 
 export default Start;

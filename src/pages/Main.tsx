ec51c0912ab83b263d3f46e91a1e4e7715cb4477
Yeojun-Y <ikm3256@naver.com>
2023-02-02 00:28:26 +0900

    feat: 네이버맵 연동

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,17 +1,44 @@
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import React, {useCallback} from 'react';
-import {Pressable, Text, View, Button, Alert, StyleSheet } from 'react-native';
+import {
+  Pressable,
+  Text,
+  View,
+  Button,
+  Alert,
+  StyleSheet,
+  Dimensions,
+} from 'react-native';
+import NaverMapView from 'react-native-nmap';
 import {RootStackParamList} from '../../App';
 
 type ScreenProps = NativeStackScreenProps<RootStackParamList, 'SignUp'>;
 
 function Main({navigation}: ScreenProps) {
   return (
-    <>
-      <View>
-        <Text style = {{alignSelf:'center', marginTop:220 ,fontSize: 60}}>메인 화면</Text>
+    <View>
+      <View
+        // eslint-disable-next-line react-native/no-inline-styles
+        style={{
+          width: Dimensions.get('window').width - 30,
+          height: 200,
+          marginTop: 10,
+        }}>
+        <NaverMapView
+          style={{width: '100%', height: '100%'}}
+          zoomControl={false}
+          center={{
+            zoom: 10,
+            tilt: 50,
+            latitude: 37.380887648,
+            longitude: 126.6679412,
+          }}
+        />
+        {/* <Text style={{alignSelf: 'center', marginTop: 220, fontSize: 60}}>
+          메인 화면
+        </Text> */}
       </View>
-    </>
+    </View>
   );
 }
 

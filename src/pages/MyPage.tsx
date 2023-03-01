6df9858b584453148bfa65e49d41ac5dec76627b
Yeojun-Y <ikm3256@naver.com>
2023-03-02 01:13:17 +0900

    update: 설정페이지로 이동

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,7 +1,22 @@
 import {View, Text} from 'react-native';
-import React from 'react';
-
-function MyPage() {
+import React, {useEffect} from 'react';
+// import {useUserContext} from '../components/UserContext';
+import {useNavigation} from '@react-navigation/native';
+import IconRightButton from '../components/IconRightButton';
+function MyPage({navigation}: any) {
+  // const {user} = useUserContext();
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <IconRightButton
+          name="gear"
+          size={35}
+          color={'black'}
+          onPress={() => navigation.navigate('Setting')}
+        />
+      ),
+    });
+  });
   return (
     <View>
       <Text>MyPage</Text>

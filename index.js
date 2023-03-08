3c5740b153f7017650e72fd2762bce242adb2364
Yeojun-Y <ikm3256@naver.com>
2023-03-08 23:35:23 +0900

    test: 구독 메시지 테스트

--- index.js
+++ index.js
@@ -11,7 +11,31 @@ messaging().setBackgroundMessageHandler(async remoteMessage => {
   console.log('Message handled in the background!', remoteMessage);
 });
 
-messaging().onNotificationOpenedApp(async remoteMessage => {
-  console.log('Massage handled in the background!', remoteMessage);
+messaging().registerDeviceForRemoteMessages();
+
+messaging().onNotificationOpenedApp(remoteMessage => {
+  console.log(
+    'Notification caused app to open from background state:',
+    remoteMessage.notification,
+  );
 });
+messaging()
+  .getInitialNotification()
+  .then(remoteMessage => {
+    if (remoteMessage) {
+      console.log(
+        'Notification caused app to open from quit state:',
+        remoteMessage.notification,
+      );
+    }
+  });
+// FCM 주제에 대한 구독자 추가
+messaging()
+  .subscribeToTopic('yjGoo1')
+  .then(() => {
+    console.log('test 토픽 구독 완료');
+  })
+  .catch(error => {
+    console.log('test 토픽 구독 실패:', error);
+  });
 AppRegistry.registerComponent(appName, () => App);

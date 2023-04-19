9fa2ec910b9f4d16bc571a2a0577e321e50de58c
Yeojun-Y <ikm3256@naver.com>
2023-04-20 03:13:24 +0900

    feat: splash화면추가

--- App.tsx
+++ App.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Provider} from 'react-redux';
 import store from './src/store';
 import AppInner from './AppInner';
+import SplashScreen from 'react-native-splash-screen';
 
 function App() {
-  // TODO: isLoggedIn = useSelector 이용하여 상태관리
-  // const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      SplashScreen.hide();
+    }, 5000);
+    return () => clearTimeout(timeoutId);
+  }, []);
+
   return (
     <>
       <Provider store={store}>
